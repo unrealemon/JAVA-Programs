67a77d1e7541dd815c1c30293ef97bf2524c10eb
Md. Emon Ali <emonali.w@gmail.com>
2023-04-04 03:51:58 +0600

    Add files via upload

---
+++ basic.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+ public static void main(String[] args) {
+      int A, B, X;
+      Scanner sc = new Scanner(System.in);
+      A = sc.nextInt();                    
+      B = sc.nextInt();                    
+      X = A + B;                           
+      System.out.print("X = "+X+"\n");    
+   }
+}
