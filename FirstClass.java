91d2aa7d37ed15c4d609307323cf9a3ccee59f41
Md. Emon Ali <emonali.w@gmail.com>
2023-08-01 00:11:27 +0600

    Add files via upload

---
+++ FirstClass.java
@@ -0,0 +1,11 @@
+package com.java.JavaProject1;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
