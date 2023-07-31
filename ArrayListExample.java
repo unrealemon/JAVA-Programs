82ec974238d47ddc302f4939409efcf4c0ca6a0e
Md. Emon Ali <emonali.w@gmail.com>
2023-08-01 00:14:23 +0600

    Add files via upload

---
+++ ArrayListExample.java
@@ -0,0 +1,18 @@
+package com.java.JavaProject1;
+import java.util.ArrayList;
+
+public class ArrayListExample {
+
+	public static void main(String[] args) {
+	
+		ArrayList <String> names = new ArrayList <String>();
+		names.add("Emon");
+		names.add("Israt");
+		names.add("Sazid");
+		System.out.println(names);
+		System.out.println(names.size());
+		System.out.println(names.get(0));
+
+	}
+
+}
