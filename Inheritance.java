16f3f2d2cb14af1f8f151505b19ba40c0df79e7f
Md. Emon Ali <emonali.w@gmail.com>
2023-08-01 00:30:31 +0600

    Add files via upload

---
+++ Inheritance.java
@@ -0,0 +1,19 @@
+package com.java.JavaProject1;
+
+public class Inheritance extends Method{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=0, b =0;
+		Method object = new Method();
+		object.Sum(10, 20);
+		object.Sub(10, 20);
+		object.Mul(10, 20);
+		object.Div(10, 20);
+		
+		
+		
+
+	}
+
+}
