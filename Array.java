de59a146ef8fd6f255380c09a7ab51be363f409c
Md. Emon Ali <emonali.w@gmail.com>
2023-08-01 00:14:02 +0600

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,19 @@
+package com.java.JavaProject1;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] names = {"Emon","Ishrat","Sazid"};
+		System.out.println(names[0]);
+		
+		for(int i=0;i<names.length;i++)
+		{
+			System.out.println(names[i]);
+			
+		}
+			
+
+	}
+
+}
